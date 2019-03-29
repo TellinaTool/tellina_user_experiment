ce3053a726d5a4f4e25c5a46a1da03cc66f23928
Andrew Tran <andremail03@gmail.com>
2019-03-29 13:10:21 -0700

    Added model for the task interface

---
+++ functions/interface_functions.sh
@@ -0,0 +1,149 @@
+##############################################################################
+# This file contains functions that the interface will use to manage the
+# following things:
+# - Creating the mock file system for the user
+# - Timing each task
+# - Determining which task and treatment ordering for the current experiment
+# - Determine the next task to move onto and whether the experiment is over
+# - Verify the output of a task
+##############################################################################
+
+
+function start_experiment() {
+  # Enable task logging
+  preexec_functions+=(preexec_func)
+  precmd_functions+=(precmd_func)
+
+  print_treatment
+
+  echo "Task Number: $TOTAL_TASKS/$TASKS_SIZE"
+  task
+}
+
+function end_experiment() {
+  # Remove bash-preexec
+  rm -f ~/.bash-preexec.sh
+  echo "Congratulations! You have completed the interactive portion of the experiment!"
+  echo "Don't forget to remove the Chrome extension that you installed."
+  echo "Please take some time to fill out the survey here <URL> using the same username you used for the experiment"
+}
+
+# Creates the mock file system that the user will be working on
+# And moves them to that directory
+function make_fs() {
+  mkdir $FS_DIR
+  tar -xzf $REPO_DIR/fs.tgz -C $TASK_DIR
+
+  # Moves the user to this directory
+  cd $FS_DIR
+}
+
+# A simple timer for each task
+function start_timer() {
+  SECONDS=0
+
+  while [ $SECONDS -lt $TIME_LIMIT ]; do
+    sleep 1
+  done
+
+  echo "You've run out of time for the task"
+  next_task 2
+}
+
+# Prints the list of resources that the user is allowed to use based on the
+# current treatment
+function print_treatment() {
+  echo -n "For this half of the experiment you can use any online resources, man pages, "
+  if [ "$TREATMENT" == "T" ]; then
+    echo "and Tellina <URL> to help you solve the tasks."
+  else
+    echo "but you can't use Tellina to help you solve the tasks."
+  fi
+}
+
+# Sets the appropriate task variables based on the TASK_ORDER var
+# The task ordering is determine using this table
+#
+#    | |  1st  |  2nd  |
+#    |-|-------|-------|
+#    |0|`s1 T` |`s2 NT`|
+#    |1|`s2 T` |`s1 NT`|
+#    |2|`s1 NT`|`s2 T` |
+#    |3|`s2 NT`|`s1 T` |
+function determine_task_order() {
+  if [ $TASK_ORDER -eq 0 ]; then
+    CURR_TS=1
+    TREATMENT="T"
+  elif [ $TASK_ORDER -eq 1]; then
+    CURR_TS=2
+    TREATMENT="T"
+  elif [ $TASK_ORDER -eq 2]; then
+    CURR_TS=1
+    TREATMENT="NT"
+  else
+    CURR_TS=2
+    TREATMENT="NT"
+  fi
+}
+
+# See documentation for scripts/verify_task.py for more details on what it does
+function verify_task() {
+  # Verify the output of the previous command.
+  $CMDS/scripts/verify_task.py $CURR_TS $CURR_TASK $PREV_CMD
+  EXIT=$?
+}
+
+# Writes to log the results of the current task
+# Parameters:
+# $1 the STATUS of the task: 0 for abandon, 1 for success, 2 for timeout
+# Increments the current task counter and determines if we need to go to the
+# next task set
+function next_task() {
+  # stop the timer
+  kill $TIMER_PID
+  wait $TIMER_PID 2> /dev/null
+
+  write_log $CURR_RESETS $SECONDS $USER_CMD $1
+
+  # Set the reset counter back to 0 for a new task
+  CURR_RESETS=0
+
+  # Increment the number of tasks finished by the user
+  TASKS_FINISHED=$((TOTAL_TASKS+1))
+  CURR_TASK=$((CURR_TASK+1))
+
+  # If we're done with all the tasks
+  if [ "$TASKS_FINISHED" -eq "$TASKS_SIZE" ]; then;
+    end_experiment
+
+    return 1
+  fi
+
+  # otherwise we check if we need to switch the task set and the treatment
+  # And go into the second half of the experiment
+  if [ "$CURR_TASK" -eq "$TS_SIZE" ];
+    CURR_TASK=0
+    # change the task set number
+    if [ "$CURR_TS" -eq 1 ]; then
+      CURR_TS=2
+    else
+      CURR_TS=1
+    fi
+    # change the treatment code
+    if [ "$TREATMENT" == "T" ]; then
+      TREATMENT="NT"
+    else
+      TREATMENT="T"
+    fi
+
+    echo "Congratulations! You have finished the first half of the experiment!"
+    print_treatment
+  fi
+
+  echo "Task Number: $TASK_NO"
+  task
+
+  # Start the timer
+  start_timer &
+  TIMER_PID=$!
+}
---
+++ functions/user_functions.sh
@@ -0,0 +1,39 @@
+##############################################################################
+# This file contains functions that the user can use throughout the experiment
+##############################################################################
+
+
+# Resets the mock file system by removing the directory and extracting the
+# original tar again
+# Sends a POST request to the server
+# Increments the variable CURR_RESETS
+# Parameters:
+# $1: current number of resets
+function reset() {
+  CURR_RESETS=$(($CURR_RESETS+1))
+  cd $REPO_DIR
+  rm -rf $FS_DIR
+
+  source $CMD/make_fs
+
+  write_log $3
+}
+
+# Abandons the current task by doing the following
+# Invokes next_task with status 0
+function abandon() {
+  next_task 0
+}
+
+# Prints the available user commands
+function help() {
+  echo "task: prints the description of the current task."
+  echo "reset: restore the file system to its original state."
+  echo "abandon: abandon the current task."
+  echo "help: prints this help message."
+}
+
+# See documentation for scripts/get_task_description.py for more details on what it does
+function task() {
+  $CMDS/scripts/get_task_description.py $CURR_TS $CURR_TASK
+}
