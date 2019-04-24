4ba6ab42de090849f6fa04279cdf4e6c369d6faf
Andrew Tran <andremail03@gmail.com>
2019-04-23 19:30:48 -0700

    Minor redesigns
    - Implemented determine task order differently
    - Some style changes

--- client_side/.infrastructure/setup.sh
+++ client_side/.infrastructure/setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# This script takes 1 argument which is the full path to the user experiment
+# directory.
 set -a
-# Get the client experiment directory
-EXP_DIR=$PWD
-# Get the client infrastructure directory
+
+# Get the full path to the user experiment directory
+EXP_DIR=$1
+# Get the full path to the experiment's infrastructure directory
 INFRA_DIR=$EXP_DIR/$(dirname ${BASH_SOURCE[0]})
 
 # Makes sure that all the scripts are executable
@@ -11,52 +14,84 @@ chmod +x ${INFRA_DIR}/*.py
 
 # Establish tasks directories and related variables
 TASKS_SIZE=22
-TS_SIZE=11 # half of the TASKS_SIZE
 
-TASKS_DIR="$INFRA_DIR/tasks"
+TASKS_DIR="${INFRA_DIR}/tasks"
 TIME_LIMIT=300
 
-USER_OUT="$INFRA_DIR/user_out"
+USER_OUT="${INFRA_DIR}/user_out"
 
-# Establish the mock file system directory
-FS_DIR="$EXP_DIR/file_system"
+# Establish the file system directory the user will perform tasks on
+FS_DIR="${EXP_DIR}/file_system"
 
-# Establish the server configurations
-# TODO: change this to an actual host
-HOST="localhost"
-PORT="8080"
+# Establish the server information
+SERVER_HOST="https://homes.cs.washington.edu/~atran35"
+SERVER_ROUTE="/tellina_user_experiment/server_side/post_handler/post_handler.php"
 
 # Establish infrastructure variables and functions
-touch $INFRA_DIR/.{curr_task,treatment,task_order,prev_cmd}
+touch ${INFRA_DIR}/.{task_code,treatment,task_order,command}
 
-echo "1" > $INFRA_DIR/.curr_task
-echo "0" > $INFRA_DIR/.task_order
-echo "T" > $INFRA_DIR/.treatment
+echo "a" > ${INFRA_DIR}/.task_code
+echo "start task" > ${INFRA_DIR}/.command
 
-source $INFRA_DIR/interface_functions.sh
+curr_task=0
+time_elapsed=0
+status="incomplete"
+curr_task=0
 
-create_user
-make_fs
+MACHINE_NAME=$(hostname)
+read -p "Enter your UW NetID: " USER_NAME
 
-source $INFRA_DIR/user_functions.sh
+USER_ID="${USER_NAME}@${MACHINE_NAME}"
+
+# Determine the task order based on a truncated md5sum hash of the username.
+# The following table is used to determine the ordering with "s" indicating the
+# task set, and T/NT for Tellina/NoTellina
+#    | |  1st  |  2nd  |
+#    |-|-------|-------|
+#    |0|`s1 T` |`s2 NT`|
+#    |1|`s2 T` |`s1 NT`|
+#    |2|`s1 NT`|`s2 T` |
+#    |3|`s2 NT`|`s1 T` |
+case $(echo $((0x$(md5sum <<<${USER_NAME} | cut -c1) % 4))) in
+  0)
+    echo "T1N2" > ${INFRA_DIR}/.task_code
+    echo "0" > ${INFRA_DIR}/.task_order
+    echo "T" > ${INFRA_DIR}/.treatment
+    ;;
+  1)
+    echo "T2N1" > ${INFRA_DIR}/.task_code
+    echo "1" > ${INFRA_DIR}/.task_order
+    echo "T" > ${INFRA_DIR}/.treatment
+    ;;
+  2)
+    echo "N1T2" > ${INFRA_DIR}/.task_code
+    echo "2" > ${INFRA_DIR}/.task_order
+    echo "NT" > ${INFRA_DIR}/.treatment
+    ;;
+  3)
+    echo "N2T1" > ${INFRA_DIR}/.task_code
+    echo "3" > ${INFRA_DIR}/.task_order
+    echo "NT" > ${INFRA_DIR}/.treatment
+    ;;
+esac
+
+source ${INFRA_DIR}/interface_functions.sh
+source ${INFRA_DIR}/user_functions.sh
+
 
 # Install Bash preexec.
-curl --silent https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh -o $INFRA_DIR/bash-preexec.sh
-source $INFRA_DIR/bash-preexec.sh
+source ${INFRA_DIR}/bash-preexec.sh
 
 # preexec will send user commands to the server
 preexec_func() {
   # Gets all the variables needed by the infrastructure and logs the user's command
-  TASK_NO=$(cat $INFRA_DIR/.curr_task)
-  TREATMENT=$(cat $INFRA_DIR/.treatment)
+  TASK_NO=$(cat ${INFRA_DIR}/.curr_task)
+  TREATMENT=$(cat ${INFRA_DIR}/.treatment)
   TIME_STAMP=$(date +%T)
-
-  echo "$1" > $INFRA_DIR/.prev_cmd
-}
-
+echo "$1" > ${INFRA_DIR}/.prev_cmd }
 precmd_func() {
-  TIME_SPENT=$SECONDS
-  COMMAND=$(cat $INFRA_DIR/.prev_cmd)
+  TIME_SPENT=${SECONDS}
+  COMMAND=$(cat ${INFRA_DIR}/.prev_cmd)
 
   if [ $TIME_SPENT -gt $TIME_LIMIT ]; then
     next_task 2
@@ -73,6 +108,8 @@ precmd_func() {
     fi
   fi
 }
+
+make_fs
 
 # Stuff here
 echo "Welcome to the user study!"
