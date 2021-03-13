0f076546b8bfe296eb36f393c42397dc16662bab
Thomas Schweizer <tschweiz@cs.washington.edu>
2021-03-12 16:53:28 -0800

    Uniform names

--- dataset/generate_clean_stack_exchange.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python clean_stack_exchange.py data-collected/stackoverflow-top500.csv https://stackoverflow.com/questions/
-python clean_stack_exchange.py data-collected/unixlinux-top500.csv https://unix.stackexchange.com/questions/
-python clean_stack_exchange.py data-collected/superuser-top500.csv https://superuser.com/questions/
